9cf1c8c8bcfdb0e0577090d320c36d3fe0d77b02
unknown <trufanovwork@gmail.com>
2025-02-04 12:10:47 +0300

    Add IntelliJ IDEA configuration files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        String request = scanner.nextLine();
+        System.err.println(request);
     }
 }
